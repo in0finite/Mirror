830480c7de996f8b090f1b40e1dd070a3c24cfbf
in0finite <leshow816@gmail.com>
2019-07-16 23:06:20 +0200

    create script

---
+++ Assets/SyncListMemoryExhaustion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SyncListMemoryExhaustion : NetworkBehaviour
+{
+
+	public struct Item
+	{
+	    public string name;
+	    public int amount;
+	    public Color32 color;
+	}
+
+	class SyncListItem : SyncList<Item> {}
+
+	SyncListItem m_inventory;
+
+	public int numOperationsPerFrame = 1000;
+
+	static int s_lastValue = 1;
+
+
+
+    public override void OnStartServer()
+    {
+        m_inventory.Add(GetItem());
+    }
+
+    static Item GetItem()
+    {
+    	return new Item(){amount = s_lastValue++};
+    }
+
+    void Update()
+    {
+        
+        if (this.isServer)
+        {
+	    	for (int i=0; i < this.numOperationsPerFrame; i++)
+	    	{
+	    		m_inventory[0] = GetItem();
+	    	}
+    	}
+
+    }
+
+}
